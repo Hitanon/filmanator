34771e36d24c95258c478f3698873bfc5e7909a5
hitanon <hitanon>
2023-06-25 17:01:34 +0500

    added functionality for the share button

--- frontend/src/components/detailed_film_info/DetailedFilmInfo.js
+++ frontend/src/components/detailed_film_info/DetailedFilmInfo.js
@@ -9,6 +9,8 @@ import InfoIconBig from "../info_icon_big/InfoIconBig";
 import BigPoster from "../poster_big/BigPoster";
 import SquareButtonBig from "../square_button_big/SquareButtonBig";
 import SimilarMoviesSlider from "../similar_movies_slider/SimilarMoviesSlider";
+import SharePopup from "../share_popup/SharePopup";
+
 
 const ICON_HEIGHT = "45px";
 const SMALL_ICON_HEIGHT = "35px";
@@ -41,129 +43,151 @@ const DetailedFilmInfo = observer(() => {
         navigate(-1);
     };
 
+    const handleShareClick = () => {
+        setIsSharePopupOpen(true);
+    };
 
+    const handleSharePopupClose = () => {
+        setIsSharePopupOpen(false);
+    };
+
+
+    const [isSharePopupOpen, setIsSharePopupOpen] = useState(false);
     const [backwardsSrc, setBackwardsSrc] = useState('/img/backwards_button.svg');
 
     return (
-        <div className="container">
-            <div className="container-grid-1">
-                <button onClick={handleBackwardsClick} className="backwards-button"
-                    onMouseEnter={() => handleBackwardsEnter(setBackwardsSrc)}
-                    onMouseLeave={() => handleBackwardsLeave(setBackwardsSrc)}
-                >
-                    <img src={backwardsSrc} alt="Backward" />
-                </button>
-                <div className="poster">
-                    <BigPoster imageUrl={filmInfoStore.currentMovie.posterUrl} />
-                </div>
-                <div className="rate-buttons-big-container">
-                    <SquareButtonBig icon="/img/like_icon.svg" size={BIG_SQUARE_BUTTON_SIZE} alt_text="like" />
-                    <SquareButtonBig icon="/img/dislike_icon.svg" size={BIG_SQUARE_BUTTON_SIZE} alt_text="dislike" />
-                    <SquareButtonBig icon="/img/share_icon.svg" size={BIG_SQUARE_BUTTON_SIZE} alt_text="share" />
-                </div>
-            </div>
-
-
-            <div className="container-grid-2">
-                <h1 className="h1">{filmInfoStore.currentMovie.title}</h1>
-                <h2 className="h2">{filmInfoStore.currentMovie.alternativeTitle}</h2>
-                <div className="icons-row">
-                    <InfoIconBig
-                        icon="/img/time_icon.svg"
-                        text={filmInfoStore.currentMovie.durationFormatted}
-                        color='#FFFFFF' height={SMALL_ICON_HEIGHT}
-                        fontSize={SMALL_ICON_FONT_SIZE} />
-                    <InfoIconBig text={filmInfoStore.currentMovie.ageRatingString}
-                        color='#FFFFFF'
-                        height={SMALL_ICON_HEIGHT}
-                        fontSize={SMALL_ICON_FONT_SIZE} />
-                </div>
-                <h3 className="h3">{filmInfoStore.currentMovie.shortDescription}</h3>
-
-                <div className="trailer-button-container">
-                    <p className="p">Трейлер: </p>
-                    <SquareButtonBig icon="/img/youtube_icon.svg" size={SQUARE_BUTTON_SIZE} onClick={handleTrailerClick} alt_text="trailer" />
+        <>
+            {isSharePopupOpen && (
+                <>
+                    <div className="overlay" onClick={handleSharePopupClose} />
+                    <SharePopup
+                        title={filmInfoStore.currentMovie.title}
+                        link={filmInfoStore.currentMovie.link}
+                        isSeries={filmInfoStore.currentMovie.isSeries}
+                        onClose={handleSharePopupClose}
+                    />
+                </>
+            )}
+            <div className="container">
+                <div className="container-grid-1">
+                    <button onClick={handleBackwardsClick} className="backwards-button"
+                        onMouseEnter={() => handleBackwardsEnter(setBackwardsSrc)}
+                        onMouseLeave={() => handleBackwardsLeave(setBackwardsSrc)}
+                    >
+                        <img src={backwardsSrc} alt="Backward" />
+                    </button>
+                    <div className="poster">
+                        <BigPoster imageUrl={filmInfoStore.currentMovie.posterUrl} />
+                    </div>
+                    <div className="rate-buttons-big-container">
+                        <SquareButtonBig icon="/img/like_icon.svg" size={BIG_SQUARE_BUTTON_SIZE} alt_text="like" />
+                        <SquareButtonBig icon="/img/dislike_icon.svg" size={BIG_SQUARE_BUTTON_SIZE} alt_text="dislike" />
+                        <SquareButtonBig onClick={handleShareClick} icon="/img/share_icon.svg" size={BIG_SQUARE_BUTTON_SIZE} alt_text="share" />
+                    </div>
                 </div>
 
-                <div className="watch-button-container">
-                    <p className="p">Смотреть полностью: </p>
-                    <SquareButtonBig icon="/img/kinopoisk_icon.svg" size={SQUARE_BUTTON_SIZE} onClick={handleWatchClick} alt_text="link" />
+
+                <div className="container-grid-2">
+                    <h1 className="h1">{filmInfoStore.currentMovie.title}</h1>
+                    <h2 className="h2">{filmInfoStore.currentMovie.alternativeTitle}</h2>
+                    <div className="icons-row">
+                        <InfoIconBig
+                            icon="/img/time_icon.svg"
+                            text={filmInfoStore.currentMovie.durationFormatted}
+                            color='#FFFFFF' height={SMALL_ICON_HEIGHT}
+                            fontSize={SMALL_ICON_FONT_SIZE} />
+                        <InfoIconBig text={filmInfoStore.currentMovie.ageRatingString}
+                            color='#FFFFFF'
+                            height={SMALL_ICON_HEIGHT}
+                            fontSize={SMALL_ICON_FONT_SIZE} />
+                    </div>
+                    <h3 className="h3">{filmInfoStore.currentMovie.shortDescription}</h3>
+
+                    <div className="trailer-button-container">
+                        <p className="p">Трейлер: </p>
+                        <SquareButtonBig icon="/img/youtube_icon.svg" size={SQUARE_BUTTON_SIZE} onClick={handleTrailerClick} alt_text="trailer" />
+                    </div>
+
+                    <div className="watch-button-container">
+                        <p className="p">Смотреть полностью: </p>
+                        <SquareButtonBig icon="/img/kinopoisk_icon.svg" size={SQUARE_BUTTON_SIZE} onClick={handleWatchClick} alt_text="link" />
+                    </div>
+
+                    <div className="about">
+                        {filmInfoStore.currentMovie.isSeries ? 'О сериале' : 'О фильме'}
+                    </div>
+
+                    <div className="info" style={{ gridRow: "7" }}>
+                        Жанр: {filmInfoStore.currentMovie.genresList}
+                    </div>
+
+                    <div className="info" style={{ gridRow: "8" }}>
+                        Страна: {filmInfoStore.currentMovie.countriesList}
+                    </div>
+
+                    <div className="info" style={{ gridRow: "9" }}>
+                        Год: {filmInfoStore.currentMovie.year}
+                    </div>
+
+                    <div className="info" style={{ gridRow: "10" }}>
+                        Режиссер: {filmInfoStore.currentMovie.directorsList}
+                    </div>
+
+                    <div className="info" style={{ gridRow: "11" }}>
+                        Бюджет: {filmInfoStore.currentMovie.budgetString}
+                    </div>
+
+                    <div className="info" style={{ gridRow: "12" }}>
+                        Сборы: {filmInfoStore.currentMovie.feesString}
+                    </div>
+
+                    <div className="about" style={{ gridRow: "13" }}>
+                        Сюжет
+                    </div>
+
+                    <div className="plot" style={{ gridRow: "14" }}>
+                        {filmInfoStore.currentMovie.description}
+                    </div>
+
+                    <div className="similar" style={{ gridRow: "15" }}>
+                        Похожие фильмы
+                    </div>
+
+                    <div className="slider" style={{ gridRow: "16" }}>
+                        <SimilarMoviesSlider movies={filmInfoStore.currentMovieSimilarMovies} />
+                    </div>
                 </div>
 
-                <div className="about">
-                    О фильме
-                </div>
 
-                <div className="info" style={{ gridRow: "7" }}>
-                    Жанр: {filmInfoStore.currentMovie.genresList}
-                </div>
-
-                <div className="info" style={{ gridRow: "8" }}>
-                    Страна: {filmInfoStore.currentMovie.countriesList}
-                </div>
-
-                <div className="info" style={{ gridRow: "9" }}>
-                    Год: {filmInfoStore.currentMovie.year}
-                </div>
-
-                <div className="info" style={{ gridRow: "10" }}>
-                    Режиссер: {filmInfoStore.currentMovie.directorsList}
-                </div>
-
-                <div className="info" style={{ gridRow: "11" }}>
-                    Бюджет: {filmInfoStore.currentMovie.budgetString}
-                </div>
-
-                <div className="info" style={{ gridRow: "12" }}>
-                    Сборы: {filmInfoStore.currentMovie.feesString}
-                </div>
-
-                <div className="about" style={{ gridRow: "13" }}>
-                    Сюжет
-                </div>
-
-                <div className="plot" style={{ gridRow: "14" }}>
-                    {filmInfoStore.currentMovie.description}
-                </div>
-
-                <div className="similar" style={{ gridRow: "15" }}>
-                    Похожие фильмы
-                </div>
-
-                <div className="slider" style={{ gridRow: "16" }}>
-                    <SimilarMoviesSlider movies={filmInfoStore.currentMovieSimilarMovies} />
-                </div>
-            </div>
-
-
-            <div className="container-grid-3">
-                <div className="rating_icons">
-                    <InfoIconBig icon="/img/match_icon.svg"
-                        text={`${filmInfoStore.currentMovie.matchPercentage}`}
-                        color='#F66004'
-                        height={ICON_HEIGHT}
-                        fontSize={ICON_FONT_SIZE} />
-                    <InfoIconBig icon="/img/imdb_icon.svg"
-                        text={`${filmInfoStore.currentMovie.imdbRating}`}
-                        color='#FFFFFF'
-                        height={ICON_HEIGHT}
-                        fontSize={ICON_FONT_SIZE} />
-                    <InfoIconBig icon="/img/kinopoisk_icon.svg"
-                        text={`${filmInfoStore.currentMovie.kinopoiskRating}`}
-                        color='#FFFFFF'
-                        height={ICON_HEIGHT}
-                        fontSize={ICON_FONT_SIZE} />
-                </div>
-                <div className="actors-container">
-                    <h3 className="actors-title">
-                        Главные роли:
-                    </h3>
-                    <div className="actors">
-                        {filmInfoStore.currentMovie.actorsList}
+                <div className="container-grid-3">
+                    <div className="rating_icons">
+                        <InfoIconBig icon="/img/match_icon.svg"
+                            text={`${filmInfoStore.currentMovie.matchPercentage}`}
+                            color='#F66004'
+                            height={ICON_HEIGHT}
+                            fontSize={ICON_FONT_SIZE} />
+                        <InfoIconBig icon="/img/imdb_icon.svg"
+                            text={`${filmInfoStore.currentMovie.imdbRating}`}
+                            color='#FFFFFF'
+                            height={ICON_HEIGHT}
+                            fontSize={ICON_FONT_SIZE} />
+                        <InfoIconBig icon="/img/kinopoisk_icon.svg"
+                            text={`${filmInfoStore.currentMovie.kinopoiskRating}`}
+                            color='#FFFFFF'
+                            height={ICON_HEIGHT}
+                            fontSize={ICON_FONT_SIZE} />
+                    </div>
+                    <div className="actors-container">
+                        <h3 className="actors-title">
+                            Главные роли:
+                        </h3>
+                        <div className="actors">
+                            {filmInfoStore.currentMovie.actorsList}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 });
 
